372077fb306c65161b2bfd2b228049f873cd579e
Erik Nilsen <enilsen16@live.com>
2015-03-22 19:56:09 -0700

    create gem

---
+++ lib/useLESS/version.rb
@@ -0,0 +1,3 @@
+module UseLESS
+  VERSION = "0.1.0"
+end
---
+++ test/test_useLESS.rb
@@ -0,0 +1,11 @@
+require 'minitest_helper'
+
+class TestUseLESS < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::UseLESS::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
